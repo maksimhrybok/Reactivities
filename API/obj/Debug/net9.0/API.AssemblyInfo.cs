53b0aab5cb9837166f7e11d296e3a022a5bb254b
maksimhrybok <maxgribok1204@gmail.com>
2025-04-13 22:08:27 +0300

    .

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0ffb22c20628b52dbf76a783ed8debb9a4a2a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67859e3789dfa665dc3287be852e06b60a8c169e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
