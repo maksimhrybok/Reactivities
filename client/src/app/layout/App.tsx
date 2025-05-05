c775f85945a958ef1ae881b1aed10e1b4f41cd35
maksimhrybok <maxgribok1204@gmail.com>
2025-05-05 18:35:13 +0300

    Refactor client application structure and update dependencies
    - Removed old App.css and App.tsx files, replacing them with a new App.tsx in the app/layout directory.
    - Introduced a new NavBar component for improved navigation.
    - Updated main.tsx to reflect new file structure and styles.
    - Adjusted project paths in various project files to use consistent casing.
    - Updated AssemblyInfo files across Application, Domain, and Persistence projects to reflect new versioning.
    - Updated sourcelink.json files to point to the latest commit.
    - Removed unused assets and styles to clean up the project.

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import {
+  Container,
+  CssBaseline,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import axios from "axios";
+import NavBar from "./NavBar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
+
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <NavBar />
+      <Container maxWidth="xl" sx={{ marginTop: 3 }}>
+        <List>
+          {activities.map((activity) => (
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+    </>
+  );
+}
+
+export default App;
