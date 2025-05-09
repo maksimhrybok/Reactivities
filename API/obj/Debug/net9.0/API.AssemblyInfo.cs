0f312702f33946ee55d16f6fb8f00cec9c707b8a
maksimhrybok <maxgribok1204@gmail.com>
2025-05-09 17:01:59 +0300

    Add ActivityDetail component for displaying activity details
    - Implemented ActivityDetail component using MUI for styling.
    - Added props to receive activity data.
    - Included Card component to display activity title, date, and description.
    - Added buttons for editing and canceling actions.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c775f85945a958ef1ae881b1aed10e1b4f41cd35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33ec9fb76e4eb8358f7501ff515693c474298b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
