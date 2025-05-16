b8c521335755eee7275262e6878955b6349eff2e
maksimhrybok <maxgribok1204@gmail.com>
2025-05-16 19:49:37 +0300

    Add React Query for data fetching and update App component

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7c0eb94f6725366ff215d8cb314b4890067783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26aed9cbbe670f90aa1091291f9495aa5e147456")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
