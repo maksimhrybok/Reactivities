b78eecda0b0a10abc439c906f3357f09511e0c91
maksimhrybok <maxgribok1204@gmail.com>
2025-05-21 02:03:57 +0300

    added routing

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c521335755eee7275262e6878955b6349eff2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc7df28f14995f2c34f80aedff37f130b81a823")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
