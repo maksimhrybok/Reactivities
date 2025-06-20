3d3cf9a5e3e55c642886abfdc225b724a47131fa
maksimhrybok <maxgribok1204@gmail.com>
2025-06-20 13:48:28 +0300

    feat: Enhance NavBar with user authentication and welcome message

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c661bdf8eee87e42c17e8da94400bd02b0b841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a9ea801e3ff7b4f1b38fc8a68a134e0f22dec51")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,14 +1,28 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { LoginSchema } from "../schemas/loginSchema";
 import agent from "../api/agent";
 
 export const useAccount = () => {
+  const queryClient = useQueryClient();
   const loginUser = useMutation({
     mutationFn: async (creds: LoginSchema) => {
       await agent.post("/login?useCookies=true", creds);
     },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
   });
+
+  const { data: currentUser } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await agent.get<User>("/account/user-info");
+      return response.data;
+    },
+  });
+
   return {
     loginUser,
+    currentUser,
   };
 };
