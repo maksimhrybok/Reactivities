b9548b4c8a424f80b6a4c9b31e74e98b7e1a1713
maksimhrybok <maxgribok1204@gmail.com>
2025-06-23 19:01:16 +0300

    Add Infrastructure project and implement user accessor
    - Added a new Infrastructure project with a project reference to Application.
    - Implemented IUserAccessor interface for user-related operations.
    - Created UserAccessor class to retrieve user information from the context.
    - Updated API project to register IUserAccessor in the dependency injection container.
    - Modified CreateActivity command handler to utilize IUserAccessor for user retrieval.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb016e38903412c6b93ca4539aaab7a20976408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ae69cbc305e6461e283db727d5ed7942dbd5fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/UserAccessor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Security.Claims;
+using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Persistence;
+
+namespace Infrastructure;
+
+public class UserAccessor(IHttpContextAccessor httpContextAccessor,
+  AppDbContext appDbContext)
+  : IUserAccessor
+{
+  public async Task<User> GetUserAsync()
+  {
+    return await appDbContext.Users
+      .FindAsync(GetUserId())
+      ?? throw new Exception("No user is logged in");
+  }
+
+  public string GetUserId()
+  {
+    return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+      ?? throw new Exception("User not found");
+  }
+}
