39ae69cbc305e6461e283db727d5ed7942dbd5fc
maksimhrybok <maxgribok1204@gmail.com>
2025-06-23 05:06:25 +0300

    Add ActivityAttendee entity and migration
    - Created ActivityAttendee class to represent the relationship between users and activities.
    - Added migration to create ActivityAttendees table with foreign keys to Activities and AspNetUsers.
    - Defined properties for DateJoined and IsHost in the ActivityAttendee entity.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e1f170541e6b7f233c0bbb5808835c0656b540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb016e38903412c6b93ca4539aaab7a20976408")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -7,5 +7,23 @@ namespace Persistence;
 
 public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
-public required DbSet<Activity> Activities { get; set; }
+  public required DbSet<Activity> Activities { get; set; }
+  public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+
+    builder.Entity<ActivityAttendee>(x => x.HasKey(a => new { a.ActivityId, a.UserId }));
+    builder.Entity<ActivityAttendee>()
+      .HasOne(u => u.User)
+      .WithMany(a => a.Activities)
+      .HasForeignKey(a => a.UserId);
+
+    builder.Entity<ActivityAttendee>()
+      .HasOne(u => u.Activity)
+      .WithMany(a => a.Attendees)
+      .HasForeignKey(a => a.ActivityId);
+
+  }
 }
