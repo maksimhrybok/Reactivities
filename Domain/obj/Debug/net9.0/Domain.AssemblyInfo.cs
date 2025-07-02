0484a2d0af9208f9bfa2b59a94b7d164ad0b0c8e
maksimhrybok <maxgribok1204@gmail.com>
2025-07-02 03:07:23 +0300

    feat: Add Cloudinary photo service integration
    - Introduced IPhotoService interface for photo upload and deletion functionality.
    - Implemented PhotoService class to handle photo operations with Cloudinary.
    - Added CloudinarySettings class for configuration of Cloudinary credentials.
    - Created DTOs for photo upload results.
    - Updated Application and Infrastructure projects to include necessary package references.
    - Added framework reference for Microsoft.AspNetCore.App in Application and Infrastructure projects.
    - Updated project asset files and caches to reflect changes.

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37432f35b591cdd08d71b146442d690d88c42d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3b42d5d6079861310d183dacce3a77838e71a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37432f35b591cdd08d71b146442d690d88c42d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3b42d5d6079861310d183dacce3a77838e71a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
