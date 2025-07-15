6f98845e1622a4e8c0085ede1fb8cc713f97b5df
maksimhrybok <maxgribok1204@gmail.com>
2025-07-15 19:52:20 +0300

    updated the photouploadwidget

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f623fadd1e18911656aa2ff6120da7605bca6b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6423c41b071ac8ba14f0c695c8892353bf192728")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,11 +1,22 @@
 import { CloudUpload } from "@mui/icons-material";
 import { Box, Grid, Typography } from "@mui/material";
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import Cropper, { ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState<object & { preview: string }[]>([]);
+  const cropperRef = useRef<ReactCropperElement>(null);
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file as Blob),
+        })
+      )
+    );
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -41,11 +52,31 @@ export default function PhotoUploadWidget() {
         <Typography variant="overline" color="secondary">
           Resize image
         </Typography>
+        {files[0]?.preview && (
+          <Cropper
+            src={files[0]?.preview}
+            style={{ height: 300, width: "90%" }}
+            initialAspectRatio={1}
+            aspectRatio={1}
+            preview=".img-preview"
+            guides={false}
+            viewMode={1}
+            background={false}
+          />
+        )}
       </Grid>
       <Grid size={4}>
-        <Typography variant="overline" color="secondary">
-          Prewiew & upload
-        </Typography>
+        {files[0]?.preview && (
+          <>
+            <Typography variant="overline" color="secondary">
+              Preview & upload
+            </Typography>
+            <div
+              className="img-preview"
+              style={{ width: 300, height: 300, overflow: "hidden" }}
+            />
+          </>
+        )}
       </Grid>
     </Grid>
   );
