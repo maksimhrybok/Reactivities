c7f279cade6277e0004b1296cb9c53a08ed505da
maksimhrybok <maxgribok1204@gmail.com>
2025-07-22 12:37:43 +0300

    feat: add CommentHub for real-time comment functionality with SignalR

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f532054dad30f9892370d3dec281b7df2945943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc63f3163370e48141ee5fdc1dbd0fc2c327d7ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
